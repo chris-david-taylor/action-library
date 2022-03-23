9e6be4020508468e2824f789960f8067a521e39d
chris taylor <cdtaylor3@gmail.com>
2022-03-23 14:05:57 +0100

    small update

--- hello-action/index.js
+++ hello-action/index.js
@@ -1,6 +1,6 @@
 
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
