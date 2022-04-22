7ec2d81e0b7ecdd6eaea6867637d67934ba0a111
chris taylor <cdtaylor3@gmail.com>
2022-04-22 11:00:30 +0200

    include an interface

---
+++ snow-action/dist/Constants.js
@@ -0,0 +1,12 @@
+class Constants {
+     
+    constructor()
+    {
+        this.api_change_request = "api/sn_chg_rest/change";
+
+        this.http_timeout = 15000;
+
+    }
+}
+
+export default Constants;
---
+++ snow-action/dist/ServiceNow.js
@@ -0,0 +1,31 @@
+class ServiceNow extends Constants {
+
+    constructor(username, password, instanceUrl)
+     {
+        super();
+        this.instanceUrl = instanceUrl;
+        this.rest = new Rest (
+            username = username,
+            password = password ); 
+        this.responseObj;    
+    }
+
+    
+    async raise_change_request( short_description = "", type = "" )
+    {
+        var json = {}
+        json['short_description'] = short_description;
+        json['type'] = type;                
+        return await this.rest._post(`${this.instanceUrl}/${this.api_change_request}`, json);
+    }
+
+
+    async get_change_request ( number = "")
+    {
+        var json = {}
+        json['number'] = number;
+        return await this.rest._get(`${this.instanceUrl}/${this.api_change_request}?number=${number}`, json);
+    }
+}
+
+export default ServiceNow;
