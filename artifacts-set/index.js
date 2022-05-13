33c5f474c5b184c0a6018e22c907a94b9509813f
chris taylor <cdtaylor3@gmail.com>
2022-05-13 15:06:21 +0200

    debug

--- artifacts-set/index.js
+++ artifacts-set/index.js
@@ -1,8 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 (async () => {
 
     try {
-        console.log("entrypoint");
+        const cloud = core.getInput('cloud');
+        const environment = core.getInput('environment');
+        const artifacts = core.getInput('artifacts');
+
+        console.log(`cloud: ${cloud}`);
+        console.log(`environment: ${environment}`);
+        console.log(`artifacts: ${artifacts}`);
     }
     catch{
         console.log("exception!");
