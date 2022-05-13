2c5a2889e16112662d1fb002550f347e36d8a717
chris taylor <cdtaylor3@gmail.com>
2022-05-13 15:19:15 +0200

    debug

--- artifacts-set/dist/index.js
+++ artifacts-set/dist/index.js
@@ -8869,10 +8869,15 @@ const github = __nccwpck_require__(4637);
         const cloud = core.getInput('cloud');
         const environment = core.getInput('environment');
         const artifacts = core.getInput('artifacts');
+        const artifactsObj = JSON.parse(artifacts);
 
         console.log(`cloud: ${cloud}`);
         console.log(`environment: ${environment}`);
         console.log(`artifacts: ${artifacts}`);
+
+        for  (const artifact in artifactsObj ){
+            console.log(`${artifact} value is ${artifactsObj[artifact]}`);
+        }
     }
     catch{
         console.log("exception!");
