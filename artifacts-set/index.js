a7909cd145e681981803655439c8076562879d25
chris taylor <cdtaylor3@gmail.com>
2022-05-13 15:17:36 +0200

    debug

--- artifacts-set/index.js
+++ artifacts-set/index.js
@@ -7,10 +7,15 @@ const github = require('@actions/github');
         const cloud = core.getInput('cloud');
         const environment = core.getInput('environment');
         const artifacts = core.getInput('artifacts');
+        const artifactsObj = JSON.parse(artifacts);
 
         console.log(`cloud: ${cloud}`);
         console.log(`environment: ${environment}`);
         console.log(`artifacts: ${artifacts}`);
+
+        for  (const artifact in artifactsObj ){
+            console.log(`${artifact} value is ${artifactsObj[artifact]}`);
+        }
     }
     catch{
         console.log("exception!");
