e0b72af16c957814fa82a7778b823fe6e5e9f0d2
chris taylor <cdtaylor3@gmail.com>
2022-05-24 15:16:07 +0200

    commit

---
+++ environment-detect/index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+
+(async () => {
+
+    try {
+        var pluginInput = core.getInput('cloud');
+
+        
+
+    } catch {
+        console.log("exception!");
+    }
+})();
